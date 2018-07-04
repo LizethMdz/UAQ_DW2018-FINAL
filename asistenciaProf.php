56a0780a332e9b7f90a12fe8f78882b7d3c5d518
HanniaFlores <40328057+HanniaFlores@users.noreply.github.com>
2018-07-04 09:18:01 -0500

    Add files via upload

---
+++ asistenciaProf.php
@@ -0,0 +1,43 @@
+
+<?php include_once('header.php'); ?>
+
+<?php include_once('profesor.php'); ?>
+
+<!--CONTENIDO DE TODO-->
+<div class = "descripcion">
+          <div id="des">
+	   <h2>	Reporte de Alumnos </h2>
+	   <!--<input type="text" name="des" value="Inscribite a nuestras convocatorias">-->
+          </div>
+	<div class="contenido-all">
+
+		<div class = "tabla-convocatoria">
+
+			<table>
+			<thead>
+			<tr>
+				<th> Id Curso </th>
+				<th> Id Alumno </th>
+				<th> Nombre Alumno </th>
+				<th> Asistencia </th>
+				<th> Calificación </th>
+			</tr>
+			</thead>
+			<tbody>
+			<tr>
+				<td> </td>
+				<td> </td>
+				<td> </td>
+				<td> <a href="#" class="myButton"> ✎ </a> </td>
+				<td> <a href="#" class="myButton"> ✔ </a> </td>
+			</tr>
+			</tbody>
+			</table>
+
+		</div>
+
+
+    </div>
+</div>
+
+<?php include_once('footer.php'); ?>
---
+++ profesor.php
@@ -0,0 +1,36 @@
+<div class = "opciones">
+
+      <div class="contenedor-nav">
+
+
+      <ul class="menu">
+
+      <li>
+        <a href=""><i class="izq fas fa-home izq"></i>Inicio</a>
+      </li>
+
+      <li>
+        <a href="#"><i class="izq fas fa-users izq"></i>Convocatoria<i class="fas fa-chevron-down der"></i></a>
+        <ul>
+          <li>
+            <a href=""><i class="izq fas fa-level-down-alt"></i>Ver</a>
+          </li>
+        </ul>
+
+            <li>
+        <a href="#"><i class="izq fas fa-users izq"></i>Curso<i class="fas fa-chevron-down der"></i></a>
+        <ul>
+          <li>
+            <a href=""><i class="izq fas fa-level-down-alt"></i>Registro A/C</a>
+          </li>
+          </ul>   
+
+    
+      <li>
+        <a href=""><i class="izq fas fa-home izq"></i>Salir</a>
+      </li>
+    
+
+</ul>
+</div>
+</div><!--opciones-->
