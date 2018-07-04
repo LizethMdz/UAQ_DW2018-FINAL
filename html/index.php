7f86a24984b1161f93f67c27040c504e4ed7e471
Unknown <analauraa.planet@gmail.com>
2018-07-03 20:31:35 -0500

    Se agregaron carpetas, cambios en las vistas de index, login y registro

---
+++ html/index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> EduCon </title>
+	<meta charset = "utf-8">
+	<link rel = "stylesheet" type = "text/css" href="estilos.css">
+	<link href="https://fonts.googleapis.com/css?family=Chewy" rel="stylesheet">
+</head>
+
+<body>
+
+
+	<div id ="menu-superior"> 
+			<div id = "menu-ingreso">
+				<div class="centro">
+					<!--<div class = "img">
+						<img src="img.jpg">
+					</div>-->
+					<div id = "logo"> 
+						<span class = "rojo"> E </span>
+						<span class = "rosa"> d </span>
+						<span class = "naranja"> u  </span>
+						<span class = "verde" > C </span>
+						<span class = "azul"> o </span>
+						<span class = "amarillo"> n </span>
+					</div>
+					<!--<div class = "img2">
+						<img src="img2.png">
+					</div>-->
+				</div><!--centro-->
+
+				<div class = "opciones"> 
+            			<div > <a href="index.php"> Inicio </a></div>
+            			<div > <a href="#"> Convocatorias </a></div>
+            			<div > <a href="#"> Contacto </a></div>
+            			<div > <a href="#"> Login </a></div>
+            			<div > <a href="#"> Registro </a></div> 
+				</div><!--opciones-->
+
+			
+				<div class = "descripcion">
+					<div id="des">
+						<h2>Inscribete a nuestras convocatorias</h2>
+						<!--<input type="text" name="des" value="Inscribite a nuestras convocatorias">-->
+					</div>
+					<div id="texto">
+						<p>Por fin despues de mucho tiempo de preparación! se lanza un programa de convocatorias con una gran variedad de cursos en distintas partes del pais para fomentar el crecimiento de la educación en la sociedad. Para poder unirte a algun curso que te interese necesitas:</p><br>
+						<h3>¡Registrarte!</h3><br>
+						<p>Solo tienes que llenar el formulario con tus datos para poder registrarte como alumno! Despues de eso podrás checar las convocatorias disponibles y podras inscribirte al curso que más te interese!</p><br>
+						<h3>¡Te deseamos mucha suerte en tu nueva aventura del conocimiento!</h3>
+					</div>
+				</div>
+			</div><!--Menu ingreso-->
+	</div><!--Menu superior-->
+  <script src="jquery/jquery-3.3.1.min.js"></script>
+  <script src="jquery/jquery.min.js"></script>
+  <script type="text/javascript" src="js/funciones.js"></script>
+</body>
+
+</html>
+</body>
+
+</html>
