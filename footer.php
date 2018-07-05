2d463f77be4193badfc7c9945aca27b9dea73c21
Lizeth_Mdz <lizethailet@hotmail.com>
2018-07-04 23:23:11 -0500

    ELINIMACION DE LOS ARCHIVOS ANTERIOES

--- footer.php
+++
@@ -1,8 +0,0 @@
-</div><!--Menu ingreso-->
-</div><!--Menu superior-->
-  <script src="jquery/jquery-3.3.1.min.js"></script>
-  <script src="jquery/jquery.min.js"></script>
-  <script type="text/javascript" src="js/funciones.js"></script>
-</body>
-
-</html>
